f145b548181601d90a293aaa6b9d1c8b0aaf0407
pietimer <dlin098123@gmail.com>
2019-03-28 16:42:30 -1000

    added some basic setup stuff

--- demo_script.R
+++ demo_script.R
@@ -1,1 +1,25 @@
 # make it work
+
+# print working directory (You should be in ~/Datatransform2)
+cat(paste("Your current working directory is: ",
+          getwd(), 
+          sep = "\n"))
+
+# bring in the Austronesian Comparative Dictionary (Proto Language Data)
+acd_data <- read.csv("acdwebscrape.csv")
+
+# testing function to split up strings and count them
+sapply(
+  strsplit(
+    toString(
+      acd_data$token_data[3333]),
+    " "),
+  length)
+
+sapply(
+  strsplit(
+    toString(
+      acd_data$gloss_data[3333]),
+    " "),
+  length)
+
