69cd9ad162910e0996eae92fb346c7ff0ee083db
pietimer <dlin098123@gmail.com>
2019-03-28 20:03:52 -1000

    mild rearranging

--- demo_script.R
+++ demo_script.R
@@ -8,16 +8,22 @@ cat(paste("Your current working directory is: ",
 #bring in the Phoible inventories data
 inventories <- read.csv("Contributions.csv")
 
-#use mutate to add a column that calculates the ratio of consonants to vowels
-ratio <- mutate(inventories, cvratio = count_consonant / count_vowel)
-
-#arrange the rows by the new column values in descending order
-ratio_ordered <- arrange(ratio, desc(cvratio))
-
 # bring in the Austronesian Comparative Dictionary (Proto Language Data)
 acd_data <- read.csv("acdwebscrape.csv")
 
+#use mutate to add a column that calculates the ratio of consonants to vowels
+ratio <- mutate(inventories, 
+                cvratio = count_consonant / count_vowel)
+
+#arrange the rows by the new column values in descending order
+ratio_ordered <- arrange(ratio, 
+                         desc(cvratio)
+                         )
+
 # make a test grouping by token and gloss
-testgroup <- group_by(acd_data, token_data, gloss_data)
+testgroup <- group_by(acd_data, 
+                      token_data, 
+                      gloss_data)
+
 # summarise (remove duplicates) in the grouping
 test_sum <- summarise(testgroup)
