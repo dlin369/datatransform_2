27727c00a1bc7648b45b7c83165a86150f0b97ab
Christina Truong <cltruong@hawaii.edu>
2019-03-28 19:09:36 -1000

    added mutate on phoible data

--- demo_script.R
+++ demo_script.R
@@ -5,6 +5,15 @@ cat(paste("Your current working directory is: ",
           getwd(), 
           sep = "\n"))
 
+#bring in the Phoible inventories data
+inventories <- read.csv("Contributions.csv")
+
+#use mutate to add a column that calculates the ratio of consonants to vowels
+ratio <- mutate(inventories, cvratio = count_consonant / count_vowel)
+
+#arrange the rows by the new column values in descending order
+ratio_ordered <- arrange(ratio, desc(cvratio))
+
 # bring in the Austronesian Comparative Dictionary (Proto Language Data)
 acd_data <- read.csv("acdwebscrape.csv")
 
