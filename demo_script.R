bee559efb6f0bba45d31b0a77310eb187a405d20
pietimer <dlin098123@gmail.com>
2019-03-28 17:34:16 -1000

    delete unnecessary code

--- demo_script.R
+++ demo_script.R
@@ -8,13 +8,6 @@ cat(paste("Your current working directory is: ",
 # bring in the Austronesian Comparative Dictionary (Proto Language Data)
 acd_data <- read.csv("acdwebscrape.csv")
 
-# testing function to split up strings and count them
-sapply(
-  strsplit(
-    toString(
-      acd_data$token_data[3333]),
-    " "),
-  length)
 
 # make a test grouping by token and gloss
 testgroup <- group_by(acd_data, token_data, gloss_data)
